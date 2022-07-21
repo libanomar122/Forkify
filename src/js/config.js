412e4c90d257281cebb35c439e2d78595e7e08c9
libanomar122 <47131321+libanomar122@users.noreply.github.com>
2022-07-20 21:14:43 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5a9f45d3-6ccb-4fc2-8cdc-08edbaac469c';
+export const MODAL_CLOSE_SEC = 2.5;
